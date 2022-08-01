64707472896a2e9ea91cf15439dd36eea5ff2de3
周兴杰 <1660154581@qq.com>
2022-08-01 16:13:30 +0800

    制作 Chart3

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import './home.scss';
 import headerBg from '../images/header.png';
 import {Chart1} from '../components/chart-1';
 import {Chart2} from '../components/chart-2';
+import {Chart3} from '../components/chart-3';
 
 const year = new Date().getFullYear();
 export const Home = () => {
@@ -14,7 +15,9 @@ export const Home = () => {
           <Chart1/>
           <Chart2/>
         </section>
-        <section className="bordered section2"></section>
+        <section className="section2">
+          <Chart3/>
+        </section>
         <section className="bordered section3"></section>
         <section className="bordered section4"></section>
         <section className="bordered section5"></section>
