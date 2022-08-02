c2560eef29fdeef23ddb98194c2ae8ec3dadcf30
周兴杰 <1660154581@qq.com>
2022-08-02 09:03:10 +0800

    消除省名，以防误会。

--- src/components/chart-6.tsx
+++ src/components/chart-6.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useRef} from 'react';
 import * as echarts from 'echarts';
 import {createEchartsOptions} from '../shared/create-echarts-options';
-import {px} from '../shared/px';
 import china from '../geo/china.json';
 
 
